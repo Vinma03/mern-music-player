40b191262256040aa5ea87b0e52f177f96808b0a
Vinma03 <140034337+Vinma03@users.noreply.github.com>
2024-05-14 22:21:48 +0530

    Update dbConnection.js

--- mp/server/config/dbConnection.js
+++ mp/server/config/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
 	try {
-		const connect = await mongoose.connect('mongodb+srv://cherpanath2105023:Vinma03@cluster0.xc2xzn0.mongodb.net/us');
+		const connect = await mongoose.connect('mongodburl');
 		console.log("DATABASE CONNECTED", connect.connection.name);
 	} catch (error) {
 		console.log(error);
